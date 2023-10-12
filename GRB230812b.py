357d70b960ed434ad0fa8ece902e1478a6ae0a3b
ThibeauWouters <thibeauwouters@gmail.com>
2023-10-12 13:26:38 +0200

    latest version KN models with jax

--- GRB230812b.py
+++ GRB230812b.py
@@ -2,22 +2,29 @@ import numpy as np
 
 # Results from run 29/09/2023
 
-Z_LANL = -83.284
-Z_Ka = -83.686
-Z_Bu = -83.584
-Z = -83.701
-Z_SN = -60.207
+
 
 # Bayes factors: preference wrt base model being just GRB:
 
-Z_list = [Z_LANL, Z_Bu, Z_Ka, Z_SN]
-names = ["LANL", "Bulla", "Kasen", "SN"]
+Z_list = [-93.278, -42.866, -92.718, -55.997, -94.767, -61.447]
+names = ["Powerlaw", "Powerlaw-SN", "Gauss", "Gauss-SN", "Tophat", "Tophat-SN"]
 
-for i, name in enumerate(names):
-    z = Z_list[i]
-    print(f"Bayes factor {name} is {np.round(z - Z, 4)}")
+base_idx = np.argmax(Z_list)
+
+base_Z = Z_list.pop(base_idx)
+base_name = names.pop(base_idx)
+
+Z_list = np.array(Z_list)
+names = np.array(names)
+
+sort_idx = np.argsort(Z_list)[::-1]
+
+Z_list = Z_list[sort_idx]
+names = names[sort_idx]
+
+# Print to screen
+print(f"Best performing: {base_name}")
+print(f"Bayes factors:")
+for z, name in zip(Z_list, names):
+    print(f"   - {name}: {np.round(z - base_Z, 4)}")
     
-# Bayes factor LANL is 0.417
-# Bayes factor Bulla is 0.117
-# Bayes factor Kasen is 0.015
-# Bayes factor SN is 23.494
